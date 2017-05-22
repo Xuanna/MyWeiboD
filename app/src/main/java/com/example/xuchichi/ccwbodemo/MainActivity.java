66bf7a30f32619b2d726b241a7916bdd7ba4f2cb
xuchichi <xuchichi@qh-speed.com>
2017-05-22 15:00:24 +0800

    注释语句

---
+++ app/src/main/java/com/example/xuchichi/ccwbodemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.xuchichi.ccwbodemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.chichi.ccwbodemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
